d6a1054a4850d643196e0f7e325be1fc598158c8
Shazeen Fabius <Shazeen-Fabius@ShazeenabiusMBP.lan>
2020-11-03 11:29:18 -0800

    completeing the missing features - search bar and the like button

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -3,10 +3,10 @@ import Comments from '../Comments/Comments';
 import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
-const Post = props => {
+const Post = (props) => {
   // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
-
+  console.log(post);
   return (
     <div className='post-border'>
       <PostHeader
@@ -21,11 +21,12 @@ const Post = props => {
         />
       </div>
       {/* Is LikeSection getting all the props it needs to work correctly? */}
-      <LikeSection likePost={() => likePost(post.id)} />
+      <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes}/>
       {/* Comments also wants its props! */}
-      <Comments />
+      <Comments comments={post.comments}/>
     </div>
   );
+  
 };
 
 export default Post;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -5,11 +5,18 @@ import './Posts.css';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
-
+  // console.log(posts)
+  // console.log(likePost)
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map((p, index)=>{
+        // console.log(index)
+        // console.log(p)
+        return <Post key={index} post={p} likePost={likePost}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
+      
     </div>
   );
 };
